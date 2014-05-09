840999e7ce6fb0b59b4e2fce0beeacd841086a3e
Gene Cho <genec2008@gmail.com>
2014-05-08 22:00:57 -0700

    WIP

--- Week1/Quiz1.R
+++ Week1/Quiz1.R
@@ -1,12 +1,15 @@
 
 
 
-if (!file.exists("data")) {
-        dir.create("data")
-}
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
-download.file(fileUrl, destfile = "./data/housing.csv", method ="curl")
-dateDownloaded <- date()
-fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf"
-download.file(fileUrl, destfile = "./data/codebook.pdf", method ="curl")
+# if (!file.exists("data")) {
+#         dir.create("data")
+# }
+# fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2Fss06hid.csv"
+# download.file(fileUrl, destfile = "./data/housing.csv", method ="curl")
+# dateDownloaded <- date()
+# fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FPUMSDataDict06.pdf"
+# download.file(fileUrl, destfile = "./data/codebook.pdf", method ="curl")
 
+data <- read.csv("./data/housing.csv")
+
+bighouses <- subset(data, VAL == 24)  # House values > $1M is code 24
