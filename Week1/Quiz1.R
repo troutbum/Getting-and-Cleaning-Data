d59d59c135dd2e5a4592acbcb4b73580e8497eaf
troutbum <genec2008@gmail.com>
2014-05-09 18:20:51 -0700

    SBUX Barbur

--- Week1/Quiz1.R
+++ Week1/Quiz1.R
@@ -13,3 +13,14 @@
 data <- read.csv("./data/housing.csv")
 
 bighouses <- subset(data, VAL == 24)  # House values > $1M is code 24
+
+
+# Question 3
+#
+fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fdata%2FDATA.gov_NGAP.xlsx"
+download.file(fileUrl, destfile = "./data/NGAP.xlsx", method ="curl")
+
+library(xlsx)
+rowIndex <- 18:23
+colIndex <- 7:15
+dat <- read.xlsx("./data/NGAP.xlsx,sheetIndex=1,colIndex=colIndex,rowIndex=rowIndex")
